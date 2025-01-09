f0f2b0c5996e5474a40f9362f9b42fd396d04af5
ocansino <oscar.cansino.457@my.csun.edu>
2025-01-08 22:24:05 -0800

    TopKFrequent

--- problems.java
+++ problems.java
@@ -7,15 +7,19 @@ class problems{
         //int[] nums = { 1, 2, 3, 4};
         //System.out.println(p.hasDuplicate(nums));
 
-        String s = "jar", t = "jam";
+        //String s = "jar", t = "jam";
         //System.out.println(p.isAnagram(s,t));
 
-        int[] nums = {5,5};
-        int target = 10;
+        //int[] nums = {5,5};
+        //int target = 10;
         //System.out.println(Arrays.toString(p.twoSum(nums, target)));
 
-        String[] strs = {"act","pots","tops","cat","stop","hat"};
-        System.out.println(p.groupAnagrams(strs));
+        //String[] strs = {"act","pots","tops","cat","stop","hat"};
+        //System.out.println(p.groupAnagrams(strs));
+
+        int[] nums = {7,7};
+        int k = 1;
+        System.out.println(Arrays.toString(p.topKFrequent(nums, k)));        
 
     }
 
@@ -91,5 +95,30 @@ class problems{
         
         return new ArrayList<>(strMap.values());
         
+    }
+
+    public int[] topKFrequent(int[] nums, int k) {
+        int[] ans = new int[k];
+        
+        Arrays.sort(nums);
+        
+        HashMap<Integer, Integer> numsMap = new HashMap<>(); //num, freq
+        for (int i = 0; i < nums.length; i++) {
+            numsMap.put(nums[i], numsMap.getOrDefault(nums[i], 0) + 1);
+        }
+        
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> a[0] - b[0]);
+        for(Map.Entry<Integer, Integer> entry : numsMap.entrySet()){
+            minHeap.offer(new int[]{entry.getValue(), entry.getKey()});
+            if(minHeap.size()> k){
+                minHeap.poll();
+            }
+        }
+
+        for (int i = 0; i < k; i++) {
+            ans[i] = minHeap.poll()[1];
+        }
+
+        return ans;
     }
 }
